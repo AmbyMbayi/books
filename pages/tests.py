03a6551ca5a2c62753ddbdbce731936d1c558c97
AmbyMbayi <ambymbayi54@gmail.com>
2020-04-20 13:55:56 +0300

    form crispy added

--- pages/tests.py
+++ pages/tests.py
@@ -3,7 +3,7 @@ from django.test import SimpleTestCase
 # Create your tests here.
 from django.urls import reverse, resolve
 
-from .views import HomePageView 
+from .views import HomePageView, AboutPageView
 
 class HomePageTests(SimpleTestCase):
     def setUp(self):
@@ -25,3 +25,27 @@ class HomePageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'AboutPage')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "hi there i should not be on the page")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view= resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
